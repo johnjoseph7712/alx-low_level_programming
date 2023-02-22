4614d684a9505424648895fb72bfaa3cb52ade96
johnjoseph7712 <johnjoseph7712@gmail.com>
2023-02-22 15:46:15 -0800

    _putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,4 +11,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
